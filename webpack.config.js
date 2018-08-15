24fc2d6aa2dd86af5161aaea652926bb3392ccf0
eJay <ejay_young@163.com>
2018-08-15 23:54:12 +0800

    chore(package.json): add build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const __DEV__ = process.env.NODE_ENV === 'development';
-const filename = __DEV__ ? '[name].js' : '[name].min.js';
-
-module.exports = {
-  entry: {
-    eui: path.join(__dirname, 'lib/index.js'),
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename,
-    library: 'eui',
-    libraryTarget: 'umd',
-  },
-  externals: {
-    react: {
-      root: 'React',
-      var: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
+module.exports = (env) => {
+  const filename = !!env.production ? '[name].min.js' : '[name].js';
+  return {
+    entry: {
+      eui: path.join(__dirname, 'lib/index.js'),
     },
-    'react-dom': {
-      root: 'ReactDOM',
-      var: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename,
+      library: 'eui',
+      libraryTarget: 'umd',
+    },
+    externals: {
+      react: {
+        root: 'React',
+        var: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        var: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      }
+    },
+    module: {},
+    plugins: [
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+      new webpack.SourceMapDevToolPlugin({
+        filename: `${filename}.map`
+      })
+    ],
+    optimization: {
+      minimize: true,
     }
-  },
-  module: {},
-  plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.SourceMapDevToolPlugin({
-      filename: `${filename}.map`
-    })
-  ],
-  optimization: {
-    minimize: true,
   }
 }
 
